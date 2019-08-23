f783d0c9711fb27768d1b1077035840316874f86
Robert Thompson <rbtprograms@gmail.com>
2019-08-23 06:59:53 -0700

    essentially mocking up data in the server

--- server.go
+++ server.go
@@ -5,6 +5,18 @@ import (
 	"net/http"
 )
 
+//PlayerServer currently only returns 20
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20\n")
+	player := r.URL.Path[len("/players/"):]
+	fmt.Fprintf(w, getPlayerScore(player))
+}
+
+func getPlayerScore(name string) string {
+	var value string
+	if name == "Pepper" {
+		value = "20"
+		} else {
+		value = "10"
+	}
+	return value
 }
--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,12 +13,27 @@ func testGETPlayers(t *testing.T) {
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
-
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
+	
+	t.Run("returns Floyds score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response:= httptest.NewRecorder()
+		
+		PlayerServer(response, request)
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	route := fmt.Sprintf("/players/%s", name)
+	req, _ := http.NewRequest(http.MethodGet, route, nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
 }
